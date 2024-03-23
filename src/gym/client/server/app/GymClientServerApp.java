b9fafe526472e3945a7c81c0fe1c8a8f24cdb09a
Daniel Barros <daniel.barros@cqumail.com>
2024-03-23 17:09:38 +0800

    Initial commit.

---
+++ src/gym/client/server/app/GymClientServerApp.java
@@ -0,0 +1,9 @@
+package gym.client.server.app;
+
+public class GymClientServerApp {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello from Java App.");
+    }
+}
