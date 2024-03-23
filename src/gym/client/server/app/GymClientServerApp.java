b140dce56a7f2ec160c4612c9be98bfe515bb61e
Daniel Barros <daniel.barros@cqumail.com>
2024-03-23 23:42:32 +0800

    Add TCP Server data saving to file.

--- src/gym/client/server/app/GymClientServerApp.java
+++ src/gym/client/server/app/GymClientServerApp.java
@@ -1,24 +1,10 @@
 package gym.client.server.app;
 
 public class GymClientServerApp {
-    private static int nextMemberNumber = 1;
-
     public static void main(String[] args) {
         System.out.println("LOG: Application started.");
         
-        // Start application servers
-        TCPServer.main(args);
-    }
-
-    public static int getNextMemberNumber() {
-        return nextMemberNumber;
-    }
-
-    public void setNextMemberNumber(int nextMemberNumber) {
-        this.nextMemberNumber = nextMemberNumber;
-    }
-    
-    public static void increaseNextMemberNumber() {
-        nextMemberNumber += 1;
+        // Start application servers, if needed
+        // TCPServer.main(args);
     }
 }
--- src/gym/client/server/app/TCPServer.java
+++ src/gym/client/server/app/TCPServer.java
@@ -4,6 +4,8 @@ import java.net.*;
 import java.io.*;
 
 public class TCPServer {
+    
+    static int nextMemberNumber = 1;
 
     public static void main(String args[]) {
         System.out.println("LOG: TCP Server started.");
@@ -27,14 +29,14 @@ class Connection extends Thread {
     DataInputStream in;
     DataOutputStream out;
     Socket clientSocket;
+    String filename;
 
     public Connection(Socket aClientSocket) {
         try {
             clientSocket = aClientSocket;
-            in = new DataInputStream(
-                    clientSocket.getInputStream());
-            out = new DataOutputStream(
-                    clientSocket.getOutputStream());
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+            filename = "memberlist.txt";
             this.start();
         } catch (IOException e) {
             System.out.println("Connection:" + e.getMessage());
@@ -42,20 +44,44 @@ class Connection extends Thread {
     }
 
     public void run() {
-        try { // an echo server
+        try {
+            // Receive message from client
+            System.out.println("Receiving data from client: " 
+                    + TCPServer.nextMemberNumber);
             String data = in.readUTF();
-            // System.out.println(data);
-            out.writeUTF("Server received: " + data);
 
+            // Write message data to txt file
+            if (data != null) {
+//                System.out.println(data);
+                writeMemberDataToFile(data);
+            }
+
+            // Send reponse to client
+            out.writeUTF("Save Data of the member number");
+            
+            // Add to server member counter
+            TCPServer.nextMemberNumber += 1;
         } catch (EOFException e) {
-            System.out.println("EOF:" + e.getMessage());
+            System.out.println("EOF: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("IO:" + e.getMessage());
+            System.out.println("IO: " + e.getMessage());
         } finally {
             try {
                 clientSocket.close();
-            } catch (IOException e) {/* close failed */
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private void writeMemberDataToFile(String data) {
+        try {
+            FileWriter fw = new FileWriter(filename, true);
+            fw.write(data + "\n");
+            System.out.println("Data received from client saved into " + filename);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
