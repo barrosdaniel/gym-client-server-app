e5a44680fb25d45b8c001f3d0e381c7d36652063
Daniel Barros <daniel.barros@cqumail.com>
2024-03-24 19:20:19 +0800

    Add routine to Serialise ArrayList objects to file.

--- src/gym/client/server/app/TCPServer.java
+++ src/gym/client/server/app/TCPServer.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class TCPServer {
-    
+
     static int nextMemberNumber = 1;
     static ArrayList<Member> allMembers = new ArrayList();
 
@@ -29,14 +29,15 @@ class Connection extends Thread {
     DataInputStream in;
     DataOutputStream out;
     Socket clientSocket;
-    String filename;
+    String fileName;
+    String objectFileName;
 
     public Connection(Socket aClientSocket) {
         try {
             clientSocket = aClientSocket;
             in = new DataInputStream(clientSocket.getInputStream());
             out = new DataOutputStream(clientSocket.getOutputStream());
-            filename = "memberlist.txt";
+            fileName = "memberlist.txt";
             this.start();
         } catch (IOException e) {
             System.out.println("Connection:" + e.getMessage());
@@ -46,7 +47,7 @@ class Connection extends Thread {
     public void run() {
         try {
             // Receive message from client
-            System.out.println("Receiving data from client: " 
+            System.out.println("Receiving data from client: "
                     + TCPServer.nextMemberNumber);
             String data = in.readUTF();
 
@@ -54,16 +55,17 @@ class Connection extends Thread {
             if (data != null) {
                 writeMemberDataToFile(data);
             }
-            
+
             // TEMP: Save data to Object file. To be replaced by timer.
-            syncObjectFile();
+            readTextFileData();
+            writeMembersToObjectFile();
             for (Member m : TCPServer.allMembers) {
                 System.out.println(m.toString());
             }
 
             // Send reponse to client
             out.writeUTF("Save Data of the member number");
-            
+
             // Add to server member counter
             TCPServer.nextMemberNumber += 1;
         } catch (EOFException e) {
@@ -81,7 +83,7 @@ class Connection extends Thread {
 
     private void writeMemberDataToFile(String data) {
         try {
-            FileWriter fw = new FileWriter(filename, true);
+            FileWriter fw = new FileWriter(fileName, true);
             fw.write(data + "\n");
             fw.close();
         } catch (IOException e) {
@@ -89,15 +91,15 @@ class Connection extends Thread {
         }
     }
 
-    private void syncObjectFile() {
-        System.out.println("Synchronising data to " + filename);
+    private void readTextFileData() {
+        System.out.println("Synchronising data to " + fileName);
         String memberLine;
         Member member;
         String[] memberData;
-        
+
         // Read contents of txt file and save to allMembers List
         try {
-            FileReader fr = new FileReader(filename);
+            FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
             while ((memberLine = br.readLine()) != null) {
                 memberData = memberLine.split(":");
@@ -111,9 +113,22 @@ class Connection extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        
-        // Write allMembers List contents to object file
-        
+    private void writeMembersToObjectFile() {
+        objectFileName = "memberlistObject";
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(objectFileName);
+            out = new ObjectOutputStream(fos);
+            for (Member member : TCPServer.allMembers) {
+                out.writeObject(member);                
+            }
+            out.close();
+            System.out.println("Member objects saved to object file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
