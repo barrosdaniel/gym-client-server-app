8fab7f8972e6229b25ff62f0f47a0ef6bf5e3c73
Daniel Barros <daniel.barros@cqumail.com>
2024-03-24 10:57:41 +0800

    Add Member class in preparation for Serialisation.

---
+++ src/gym/client/server/app/Member.java
@@ -0,0 +1,47 @@
+package gym.client.server.app;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+
+    public Member() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
